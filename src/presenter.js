535e0b774757711da9c95915b085fc0ff819eba6
oswaldoroman <edwin.roman@ucb.edu.bo>
2023-06-01 09:21:28 -0400

    Se realizan cambios en el index y en el presenter para que haga la resta y desvuelva el cambio correcto

--- src/presenter.js
+++ src/presenter.js
@@ -1,8 +1,8 @@
 import restar from "./calculador";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const first = document.querySelector("#monto-venta");
+const second = document.querySelector("#monto-efectivo");
+const form = document.querySelector("#calcular-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
@@ -11,5 +11,5 @@ form.addEventListener("submit", (event) => {
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>" + restar(secondNumber, firstNumber) + "</p>";
 });
