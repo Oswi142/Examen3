0d08240d9e79e1b1b359f7756249af9383c532fb
oswaldoroman <edwin.roman@ucb.edu.bo>
2023-06-01 09:11:41 -0400

    Se creo calculador.js y dentro de este una funcion para restar

---
+++ src/calculador.js
@@ -0,0 +1,6 @@
+function restar(a, b) {
+    return b-a;
+  }
+  
+  export default restar;
+  
--- src/presenter.js
+++ src/presenter.js
@@ -1,4 +1,4 @@
-import sumar from "./sumador";
+import restar from "./calculador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
